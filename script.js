aec755475d7737879104cb1d189de18728dce5c9
TantoluwaHeritageA <heritage1609@gmail.com>
2022-04-06 11:37:52 +0300

    day-1

---
+++ script.js
@@ -0,0 +1,10 @@
+function sumOfArray(array){
+    let sum = 0
+   for (let i = 0; i < array.length; i++) {
+     sum = sum + array[i]
+   
+   }
+       console.log(sum)
+     return sumOfArray
+   }
+   sumOfArray([10,6,7,8,9,2])
